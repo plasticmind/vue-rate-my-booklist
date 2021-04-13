c079dd3b6ec14d3b074dfd624fad2f205fc363c4
Jesse Gardner <jesse@plasticmind.com>
2021-04-13 18:33:27 -0400

    Disabled created and delete API hooks

--- functions/books.js
+++ functions/books.js
@@ -6,12 +6,14 @@ const updateBook = require('./helpers/updateBook');
 exports.handler = async (event) => {
     if (event.httpMethod === 'GET') {
         return await getBooks(event);
-    } else if (event.httpMethod === 'POST') {
-        return await createBook(event);
     } else if (event.httpMethod === 'PUT') {
         return await updateBook(event);
+/*
+    } else if (event.httpMethod === 'POST') {
+        return await createBook(event);
     } else if (event.httpMethod === 'DELETE') {
         return await deleteBook(event);
+*/
     } else {
         return formattedReturn(405, {});
     }
